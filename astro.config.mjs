970b0887de5671e8583b09aa6d718c5f88f87d51
[Marek] <marekkabala1@gmail.com>
2023-12-11 18:51:10 +0000

    vercel instaled

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	output: 'hybrid',
+	adapter: vercel({
+		maxDuration: 50,
+		includeFiles: ['./pages/og/ogGenerate.json'],
+	}),
+});
